2f12f1dbb9a520719b4e10e2bb7e47ccba643c56
Astenik <106985461+Astenik@users.noreply.github.com>
2022-06-18 12:28:04 +0400

    Update and rename homework12 to homework12.py

--- homework12.py
+++ homework12.py
@@ -1,12 +1,14 @@
 def remove_all(data, value):
     """This function removes all members of datat that are equal to value."""
-    ind = 0
+    if  not value in data:
+        return Exception('the list is not contain the value')
+    ind = 0 
     while  ind < len(data):
-         if data[ind] = value
+         if data[ind] == value:
              data.remove(ind)
-          ind += 1
-     return  data
+         ind += 1
+    return  data
      
 data = [9, 6, 13, 9]
-print(remove_all(data))
+print(remove_all(data, 9))
       
