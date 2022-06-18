03bda9033efddbccda5d0a6a6b680ce7cb795301
Astenik <106985461+Astenik@users.noreply.github.com>
2022-06-18 12:41:29 +0400

    Update and rename homework3 to homework3.py

--- homework3.py
+++ homework3.py
@@ -1,12 +1,12 @@
 def  pow(base, exp):
     """This function returns the exp pow of base"""
     if base > 0 and exp > 0:
-        return base**exp
-    elif base > 0 and exp==0
+        return base ** exp
+    elif base > 0 and exp==0:
         return 1
-    elif base > 0 and exp < 0
-          return 1/base**(-exp)
-     elif (base == 0 and exp < 0) or base < 0:
+    elif base > 0 and exp < 0:
+        return 1 / base ** (-exp)
+    elif (base == 0 and exp < 0) or base < 0:
          return -1
          
          
