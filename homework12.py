06f2e31a545fb3762bd8b23d5dc7bc01b2d2e476
Astenik <106985461+Astenik@users.noreply.github.com>
2022-06-18 12:29:42 +0400

    Update homework12.py

--- homework12.py
+++ homework12.py
@@ -1,7 +1,7 @@
 def remove_all(data, value):
     """This function removes all members of datat that are equal to value."""
     if  not value in data:
-        return Exception('the list is not contain the value')
+        raise Exception('the list is not contain the value')
     ind = 0 
     while  ind < len(data):
          if data[ind] == value:
