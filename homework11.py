b3e1e6d51277b9dfa21fd8abe1651d83b5e3e85c
Astenik <106985461+Astenik@users.noreply.github.com>
2022-06-18 12:20:10 +0400

    Update and rename homework11 to homework11.py

--- homework11.py
+++ homework11.py
@@ -1,14 +1,15 @@
 def revers(data):
     """This function reverses the layout of the data list."""
-    data1 = []
+    data1 = [None] * len(data) - 1
     i = 0
-    j = len(data)-1
-    while j >= 0:
+    j = len(data) - 1
+    while j >= 0 and i < len(data):
        data1[i] = data[j]
        i += 1
        j -= 1
     return data1
+    t
     
-    
+data = [8, 9, 24, 9]
 print("The reveres version of data is:", revers(data))
     
