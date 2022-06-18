75f992bbe1048a869edbacec5436e18096b9597a
Astenik <106985461+Astenik@users.noreply.github.com>
2022-06-18 12:53:57 +0400

    Update and rename homework8 to homework8.py

--- homework8.py
+++ homework8.py
@@ -1,11 +1,11 @@
-def contain(data, value):
+def is_contain(data, value):
     """this function reurns true if the data list contains value else returns false"""
-    i=0
+    i = 0
     while i < len(data):
          if data[i] == value:
              return true
          i += 1
-     reurn false
+    reurn false
      
-     
- print("Whedher the data contains 5:", contain(data, 5))
+ data = [7, 8, 9, 34]
+ print("Whedher the data contains 5:", is_contain(data, 5))
