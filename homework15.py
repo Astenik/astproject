918937371298ccbf7cfaa8c7a4d48d6e5141b331
Astenik <106985461+Astenik@users.noreply.github.com>
2022-06-19 17:21:37 +0400

    Create homework15.py

---
+++ homework15.py
@@ -0,0 +1,15 @@
+def sum(num1, num2, num3):
+    """This function returns the sum of 3 umbers."""
+    return num1 + num2 + num3
+def map3(func, data1, data2, data3):
+       """This function returns new list consist of the union of 3 lists."""
+         res = [None] * len(data1)
+         for ind in range(len(data1)):
+              res[ind] = func(data1[ind], data2[ind], data3[ind])
+         return res
+    
+
+data = [34, 7, 85, 45]
+data2 = [34, 7, 85, 7]
+data3 = [34, 7, 85, 0]
+print("the new list is:", map3(sum, data, data2, data3))
